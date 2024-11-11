9a64286b526e6dcf49147baef1b13a2d3968b328
zhang Wei <136400441+cd10961@users.noreply.github.com>
2024-11-11 11:48:46 +0900

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    s3 = {
+      source  = "hashicorp/aws"
+      version = "~> 2.1.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_s3_bucket" "ssd_bucket" {
+  bucket = "trfm-ssd-s3-test-000002"
+
+  versioning {
+    enabled = true
+  }
+}
